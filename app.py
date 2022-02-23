8d475677d68168ca0f4fc3ac1b798da24ed0f0f0
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-02-23 02:17:38 +0200

    saglabā json speletaja vardu

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, request
 
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/sarauj", methods=['GET','POST'])
 def index():
-    return render_template('/index.html')
+    vards = request.args.get('vards')
+    jauns_ieraksts = {"vards": vards}
+    with open("dati/speletajs.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(jauns_ieraksts, indent=2, ensure_ascii=False))
 
 
-@app.route("/login")
+    with open("dati/top.json", "r", encoding="utf-8") as f:
+        dati = json.loads(f.read())
+        
+    return render_template('/index.html', vards=vards)
+
+@app.route("/",  methods=["GET", "POST"])
 def login():
     return render_template('/login.html')
 
-
+@app.route("/top",  methods=["GET", "POST"])
+def top():
+    return render_template('/top.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
