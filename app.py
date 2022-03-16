c1de97b8227cfeff366140b7e021833d900c3849
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-03-16 17:21:11 +0200

    starprezultats

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
 
+from importlib.resources import path
 from flask import Flask, jsonify, render_template, request
 import json
 
 app = Flask(__name__)
-
 speletaji = []
-@app.route("/sarauj", methods=['GET','POST'])
-def index():
-    vards = request.args.get('vards')
-    rezult = request.args.get('punkti')
-    speletajs = {"vards": vards,
-                "punkti": rezult}
-    with open("dati/speletajs.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps(speletajs, indent=2, ensure_ascii=False))
-    return render_template('/index.html', vards=vards)
-
-   
 
 @app.route("/",  methods=["GET", "POST"])
 def login():
     return render_template('/login.html')
-
-@app.route("/top",  methods=["GET", "POST"])
-def top():
-    with open("dati/top.json", "r", encoding="utf-8") as f:
-        top =json.loads(f.read())
-        
-        
-    return render_template('/top.html', top=top)
 
 
 @app.route('/form', methods=["POST"])
 def form():
     vards = request.form.get('vards')
     speletaji.append(f"{vards}")
-    title="Thank You!"
+    title="Paldies!"
+   
     return render_template("form.html", title=title, vards=vards, speletaji=speletaji)
 
 
+@app.route("/sakums",  methods=["GET", "POST"])
+def sakums():
+    return render_template('/sakums.html', speletaji=speletaji)
 
-[speletaji]
+@app.route("/help",  methods=["GET", "POST"])
+def help():
+    return render_template('/sakums.html' )
 
+
+@app.route("/top",  methods=["GET", "POST"])
+def top():
+    jauns_speletajs = request.form.get('vards')
+    with open("dati/top.json", "r", encoding="utf-8") as f:
+        TOP =json.loads(f.read())
+        
+   
+        
+    return render_template('/top.html', TOP=TOP)
+
+
+        
 if __name__ == "__main__":
     app.run(debug = True)
 
