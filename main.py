44019b8b8efe1487f868fd2bdc6662136e5515b4
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-03-21 17:36:14 +0200

    .

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 from importlib.resources import path
 from flask import Flask, jsonify, render_template, request
 import json
 
 app = Flask(__name__)
 speletaji = []
+rindas = []
 
 @app.route("/",  methods=["GET", "POST"])
 def login():
@@ -16,7 +16,9 @@ def form():
     vards = request.form.get('vards')
     speletaji.append(f"{vards}")
     title="Paldies!"
-   
+    with open('dati/chats.json', 'w', encoding="utf-8") as f:
+        f.write(json.dumps(vards, indent=2, ensure_ascii=False))
+        
     return render_template("form.html", title=title, vards=vards, speletaji=speletaji)
 
 
@@ -34,22 +36,74 @@ def layout():
     return render_template('/layout.html', title="layout")
 
 @app.route("/spele",  methods=["GET", "POST"])
-def lspele():
-    return render_template('/spele.html', title="layout")
+def spele():
+    return render_template('/spele.html', title="spele")
 
 @app.route("/top",  methods=["GET", "POST"])
 def top():
-    jauns_speletajs = request.form.get('vards')
-    with open("dati/top.json", "r", encoding="utf-8") as f:
-        TOP =json.loads(f.read())
-        
+    return render_template('/top.html', title="top")
+
+@app.route('/rezultati/<rezultats>/<time>')
+def spelesRezultats(rezultats, time):
+    #vards = request.form.get('vards')
+    #punkti = {'rezultats'}
+    rinda = {
+      "kopejie punkti": rezultats,
+      "atlikušais laiks": time
+    }
    
-        
-    return render_template('/top.html', TOP=TOP)
+    rindas.append(rinda)
+  
+    with open('dati/chats.json', 'w', encoding="utf-8") as f:
+        f.write(json.dumps(rindas, indent=2, ensure_ascii=False))
+    
+    return "OK"
 
+# @app.route('/sutit/<vards>/<zina>')
+# def sutit(vards, zina):
+#   tagad = datetime.now()
+#   laiks = tagad.strftime("%Y/%m/%d, %H:%M:%S")
 
-        
+  # "/sutit/Anna/Labrīt visiem!"
+#   rinda = {
+#       "zina": zina,
+#       "vards": vards,
+#       "laiks": laiks
+#   }
+#   with open('dati/chats.json', 'r', encoding="utf-8") as r:
+#     vecie = r.read()
+#     rindas = json.loads(vecie)
+
+#   if vards == "Serveris":
+#     rinda["vards"] = "Blēdis"
+  
+#   if zina == "!stats":
+#     chata_garums = len(rindas)
+#     rinda["zina"] = "Čatā ir " + str(chata_garums) + " ieraksti."
+#     rinda["vards"] = "Serveris"
+#   elif zina == "!joks":
+
+    # dati = requests.get("https://api.chucknorris.io/jokes/random")
+    # print(dati)
+    # try:
+    #   joks = dati.json()["value"]
+    #   rinda["vards"] = "Serveris"
+    #   rinda["zina"] = joks
+    # except:
+    #   print(dati.status_code)
+    #   return "Nope"
+      
+#   rindas.append(rinda)
+  
+#   with open('dati/chats.json', 'w', encoding="utf-8") as f:
+#     f.write(json.dumps(rindas, indent=2, ensure_ascii=False))
+    
+#   return "OK"
+       
 if __name__ == "__main__":
     app.run(debug = True)
 
+# if __name__ == "__main__":
+#   from waitress import serve
+#   serve(app, host="0.0.0.0", port=8080)
 
